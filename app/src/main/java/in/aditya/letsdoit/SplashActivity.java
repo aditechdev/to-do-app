0832bec0c15e2a2cca48f763111d2f8f79c97529
AdiTechDev <aditechdevofficial@gmail.com>
2021-04-15 11:21:07 +0530

    :rocket: Splash Activity Done

--- app/src/main/java/in/aditya/letsdoit/SplashActivity.java
+++ app/src/main/java/in/aditya/letsdoit/SplashActivity.java
@@ -1,6 +1,23 @@
 package in.aditya.letsdoit;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        },1000);
+
+    }
 }
